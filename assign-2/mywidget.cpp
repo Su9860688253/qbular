53963d252b21d438038f263c372231bc8eadaebf
Ian S. McBride <ian.s.mcb@gmail.com>
2014-03-06 18:18:20 -0500

    fixed tabbing in mywidget.cpp

--- assign-2/mywidget.cpp
+++ assign-2/mywidget.cpp
@@ -15,42 +15,42 @@ MyWidget::MyWidget(QWidget *parent)
     MainWidget *mainWidget = new MainWidget();
     layoutMain->addWidget(mainWidget);
 */
-//creates group boxes
-transformGroupBox = new QGroupBox("Transform");
-countGroupBox = new QGroupBox("LED Count");
-spacingGroupBox = new QGroupBox("LED Spacing");
-colorGroupBox = new QGroupBox("LED Color");
+    //creates group boxes
+    transformGroupBox = new QGroupBox("Transform");
+    countGroupBox = new QGroupBox("LED Count");
+    spacingGroupBox = new QGroupBox("LED Spacing");
+    colorGroupBox = new QGroupBox("LED Color");
 
-//sets min and max size of group boxes
-QSize groupBoxSize(250, 125);
-transformGroupBox->setMinimumSize(groupBoxSize);
-countGroupBox->setMinimumSize(groupBoxSize);
-spacingGroupBox->setMinimumSize(groupBoxSize);
-colorGroupBox->setMinimumSize(groupBoxSize);
+    //sets min and max size of group boxes
+    QSize groupBoxSize(250, 125);
+    transformGroupBox->setMinimumSize(groupBoxSize);
+    countGroupBox->setMinimumSize(groupBoxSize);
+    spacingGroupBox->setMinimumSize(groupBoxSize);
+    colorGroupBox->setMinimumSize(groupBoxSize);
 
-transformGroupBox->setMaximumSize(groupBoxSize);
-countGroupBox->setMaximumSize(groupBoxSize);
-spacingGroupBox->setMaximumSize(groupBoxSize);
-colorGroupBox->setMaximumSize(groupBoxSize);
+    transformGroupBox->setMaximumSize(groupBoxSize);
+    countGroupBox->setMaximumSize(groupBoxSize);
+    spacingGroupBox->setMaximumSize(groupBoxSize);
+    colorGroupBox->setMaximumSize(groupBoxSize);
 
-//adds group boxes to left layout
-leftVLayout = new QVBoxLayout();
-leftVLayout->addWidget(transformGroupBox);
-leftVLayout->addWidget(countGroupBox);
-leftVLayout->addWidget(spacingGroupBox);
-leftVLayout->addWidget(colorGroupBox);
-leftVLayout->addStretch();
+    //adds group boxes to left layout
+    leftVLayout = new QVBoxLayout();
+    leftVLayout->addWidget(transformGroupBox);
+    leftVLayout->addWidget(countGroupBox);
+    leftVLayout->addWidget(spacingGroupBox);
+    leftVLayout->addWidget(colorGroupBox);
+    leftVLayout->addStretch();
 
-//creates simulation and adds it the right layout
-QSize simulationSize(500, 500);
-mainWidget = new MainWidget();
-mainWidget->setMinimumSize(simulationSize);
-rightHLayout = new QHBoxLayout();
-rightHLayout->addWidget(mainWidget);
+    //creates simulation and adds it the right layout
+    QSize simulationSize(500, 500);
+    mainWidget = new MainWidget();
+    mainWidget->setMinimumSize(simulationSize);
+    rightHLayout = new QHBoxLayout();
+    rightHLayout->addWidget(mainWidget);
 
-//adds controls and simulation to main layout
-mainHLayout = new QHBoxLayout();
-mainHLayout->addLayout(leftVLayout);
-mainHLayout->addLayout(rightHLayout);
-setLayout(mainHLayout);
+    //adds controls and simulation to main layout
+    mainHLayout = new QHBoxLayout();
+    mainHLayout->addLayout(leftVLayout);
+    mainHLayout->addLayout(rightHLayout);
+    setLayout(mainHLayout);
 }
